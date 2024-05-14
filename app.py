36f99ff4efc1c20b71cf0268a6721d51da8dcf18
Yiorgos Yiannkoulias <56363368+yiorgosynkl@users.noreply.github.com>
2024-05-14 16:00:35 +0000

    add  gnkl page

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/gnkl")
+def gnkl_ui():
+    return render_template("gnkl.html")
